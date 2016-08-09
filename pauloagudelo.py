abdc5b67502ad8e459194c94554243b4327ab53e
pauloagudelo <pagudeloj@gmail.com>
2016-08-08 20:39:05 -0500

    creacion del proyecto

---
+++ pauloagudelo.py
@@ -0,0 +1,106 @@
+
+# coding: utf-8
+
+# In[1]:
+
+print "hola mundo"
+
+
+# $$\int_x^\infty\frac{dt}{t(t^2-1)log(t)}$$
+
+# # HOLA MUNDO EN PYTHON
+# hola mundo desde python
+
+# In[4]:
+
+3*2
+
+
+# In[5]:
+
+3/2
+
+
+# In[6]:
+
+3.0/2
+
+
+# In[7]:
+
+2+4*6/8.0-3
+
+
+# In[8]:
+
+3+5j
+
+
+# In[10]:
+
+(3+5j)-(1-1j)
+
+
+# In[11]:
+
+abs(3+5j)
+
+
+# In[12]:
+
+5**3
+
+
+# In[13]:
+
+_10
+
+
+# In[14]:
+
+int(_6)
+
+
+# In[15]:
+
+float(_6)
+
+
+# In[16]:
+
+round(_11)
+
+
+# In[20]:
+
+max(1, 3, -5)
+
+
+# In[22]:
+
+x,y,z,a=1,2,3,4
+
+
+# In[23]:
+
+print x
+print y
+print z
+print a
+
+
+# In[24]:
+
+y
+
+
+# In[28]:
+
+x=5 
+y=8
+if x>y: 
+    print ("el mayor es ",x)
+else:
+        print("el menor es ",y)
+    
+
